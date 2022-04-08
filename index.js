4d9aa07de197586cb85fea3e9408af59452aeee6
Erick Vargas <erickkvargas@gmail.com>
2022-04-07 19:28:04 -0700

    commit tree 7669e458af4f0c98389ed32e10ae45da235d25c0

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const ul = document.getElementById('main')
+ul.remove();
+
+let newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.id = "victory"
+newHeader.textContent= "Erick is the champion"
+
